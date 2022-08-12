fdfc09944c6eb02d6684031d1ed9142def0280f7
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-08-12 12:37:21 +0100

    Update quiz_client.py

--- quiz_client.py
+++ quiz_client.py
@@ -4,7 +4,7 @@ from tkinter import *
 
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip_address = '127.0.0.1'
 port = 8000
@@ -66,72 +66,7 @@ class GUI:
         rcv = Thread(target=self.receive)
         rcv.start()
 
-    def layout(self,name):
-        
-        self.name = name
-        self.window.deiconify()
-        self.window.title("CHATROOM")
-        self.window.resizable(width = False,
-							height = False)
-        self.window.configure(width = 470,
-							height = 550,
-							bg = "#17202A")
-        self.labelHead = Label(self.window,
-							bg = "#17202A",
-							fg = "#EAECEE",
-							text = self.name ,
-							font = "Helvetica 13 bold",
-							pady = 5)
-        
-        self.labelHead.place(relwidth = 1)
-        self.line = Label(self.window,
-						width = 450,
-						bg = "#ABB2B9")
-        
-        self.line.place(relwidth = 1,
-						rely = 0.07,
-						relheight = 0.012)
-        
-        self.textCons = Text(self.window,
-							width = 20,
-							height = 2,
-							bg = "#17202A",
-							fg = "#EAECEE",
-							font = "Helvetica 14",
-							padx = 5,
-							pady = 5)
-        
-        self.textCons.place(relheight = 0.745,
-							relwidth = 1,
-							rely = 0.08)
-        self.textCons.config(cursor='arrow')
-        self.labelBottom = Label(self.window,bg = '#ABB2B9',height=80)
-        self.labelBottom.place(relwidth=1,rely=0.825)
-        
-        self.entryMsg = Entry(self.labelBottom,bg='#2C3E50',fg='#EAECEE',font='Helvetica 13')
-        self.entryMsg.place(relwidth=0.74,rely=0.008,relx=0.011,relheight=0.06)
-        self.entryMsg.focus()
-        
-        self.buttonMsg = Button(self.labelBottom,text='Send',font='Helvetica 10 bold',width=20,bg='#ABB2B9',command=lambda:self.sendButton(self.entryMsg.get()))
-        self.buttonMsg.place(relx=0.77,rely=0.008,relheight=0.06,relwidth=0.22)
-        
-        scrollbar = Scrollbar(self.textCons)
-        scrollbar.place(relheight=1,relx=0.974)
-        scrollbar.config(command=self.textCons.yview)
-        self.textCons.config(state=DISABLED)
-    
-    def sendButton(self,msg):
-        self.textCons.config(state=DISABLED)
-        self.msg = msg
-        self.entryMsg.delete(0,END)
-        snd = Thread(target=self.write)
-        snd.start()
-        
-    def show_message(self,message):
-        self.textCons.config(state=NORMAL)
-        self.textCons.insert(END,message+'\n\n')
-        self.textCons.config(state=DISABLED)
-        self.textCons.see(END)
+   
   
 
     def receive(self):
@@ -147,22 +82,6 @@ class GUI:
                 print('An error has occured')
                 client.close()
                 break
-		
-        
-    def sendButton(self,msg):
-        self.textCons.config(state=DISABLED)
-        self.msg = msg
-        self.entryMsg.delete(0,END)
-        snd = Thread(target=self.write)
-        snd.start()
-        
-    def write(self):
-        self.textCons.config(state=DISABLED)
-        while True:
-            message = (f"{self.name}: {self.msg}")
-            client.send(message.encode('utf-8'))
-            self.show_message(message)
-            break
         
 
 g = GUI()
