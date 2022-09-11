9039f2ac9cd9160056d3920ab722787e8dc0a5c7
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-11 19:18:45 +0100

    Update quiz_client.py

--- quiz_client.py
+++ quiz_client.py
@@ -83,20 +83,53 @@ class GUI:
                 print('An error has occured')
                 client.close()
                 break
+		
     def layout(self,name):
-	self.labelHead = Label(self.Window,bg = "#17202A",fg = "#EAECEE",text = self.name , font = "Helvetica 13 bold",pady = 5) 
-	self.labelHead.place(relwidth = 1) 
-	self.line
-	self.textCons
-	self.labelBottom
-	self.entryMsg
-	self.buttonMsg
-	self.scrollbar
 	self.name = name
-	self.Window.deiconify()
-	self.Window.title('CHAT ROOM')
-	self.Window.resizeable(width=False, height = False)
-	self.Window.configure(width = 470, height= 550, bg = '#17202A')
+        self.Window.deiconify()
+        self.Window.title("CHATROOM")
+        self.Window.resizable(width = False,height = False)
+        self.Window.configure(width = 470,height = 550,bg = "#17202A")
+        self.labelHead = Label(self.Window,bg = "#17202A",fg = "#EAECEE",text = self.name ,       font = "Helvetica 13 bold",pady = 5)
+        self.labelHead.place(relwidth = 1)
+        self.line = Label(self.Window,width = 450,bg = "#ABB2B9")        
+        self.line.place(relwidth = 1,rely = 0.07,relheight = 0.012)            
+        self.textCons = Text(self.Window,width = 20,height = 2,bg = "#17202A",fg = "#EAECEE",font = "Helvetica 14",padx = 5,pady = 5)
+        self.textCons.place(relheight = 0.745,relwidth = 1,rely = 0.08)
+        self.labelBottom = Label(self.Window,bg = "#ABB2B9",height = 80)
+        self.labelBottom.place(relwidth = 1,rely = 0.825)
+        self.entrymessage = Entry(self.labelBottom,bg = "#2C3E50",fg = "#EAECEE",font = "Helvetica 13")
+        self.entrymessage.place(relwidth = 0.74,relheight = 0.06,rely = 0.008,relx = 0.011)
+        self.entrymessage.focus()
+        self.buttonmessage = Button(self.labelBottom,text = "Send",font = "Helvetica 10 bold",width = 20,bg = "#ABB2B9",command = lambda: self.sendButton(self.entrymessage.get()))
+        self.buttonmessage.place(relx = 0.77,rely = 0.008,relheight = 0.06,relwidth = 0.22)
+        self.textCons.config(cursor = "arrow")
+        scrollbar = Scrollbar(self.textCons)
+        scrollbar.place(relheight = 1,relx = 0.974)        
+        scrollbar.config(command = self.textCons.yview)          
+        self.textCons.config(state = DISABLED)
+
 	
+    def sendButton(self, message):
+		self.textCons.config(state = DISABLED)
+		self.message=message
+		self.entrymessage.delete(0, END)
+		snd= Thread(target = self.write)
+		snd.start()
+    
+    def show_message(self, message):
+		self.textCons.config(state = NORMAL)
+		self.textCons.insert(END, message+"\n\n")
+		self.textCons.config(state = DISABLED)
+		self.textCons.see(END)
+
+
+    def write(self):
+		self.textCons.config(state=DISABLED)
+		while True:
+			message = (f"{self.name}: {self.message}")
+			client.send(message.encode('utf-8'))
+			self.show_message(message)	
+			break
 g = GUI()
 
